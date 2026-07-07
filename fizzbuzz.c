662e8114de58a8939a51e679f7d99dbbc78c43a8
noadhum <noa.adhum99@gmail.com>
2026-07-07 08:19:54 +0700

    rewrite fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,26 +2,15 @@
 
 void fizzbuzz(int n)
 {
-     int printed = 0;
-
-     if (n % 3 == 0) {
-          printf("Fizz");
-          printed = 1;
-     }
-     if (n % 5 == 0) {
-          printf("Buzz");
-          printed = 1;
-     }
-
-     if (printed) putc('\n', stdout);
+     if (n % 15 == 0) printf("FizzBuzz\n");
+     else if (n % 3 == 0) printf("Fizz\n");
+     else if (n % 5 == 0) printf("Buzz\n");
      else printf("%d\n", n);
 }
 
 int main(void)
 {
-     for (int i = 0; i <= 100; i++) {
+     for (int i = 1; i <= 100; i++) {
           fizzbuzz(i);
      }
-
-     return 0;
 }
